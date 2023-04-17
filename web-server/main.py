3047c17e697ba7e24d62c190f26c81df33c3563c
kina16 <pedroghirardi49@gmail.com>
2023-04-16 21:41:40 -0300

    Codigo de Docker

--- web-server/main.py
+++ web-server/main.py
@@ -1,5 +1,19 @@
 import store
 
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+#decorador es lo del arroba osea la linea 9 la 10 ya es parte del codigo
+@app.get("/")
+def get_list():
+    return[1,2,3,]
+
+@app.get("/contact")
+def get_list():
+    return{'name': "MIRA MARTIN HICE UNA PAGINA WEB CON PYTHON XD"}
+
 def run():
     store.get_categories()
 if __name__=='__main__':
